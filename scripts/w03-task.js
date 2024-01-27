692430496dcbf716916de2bf4432dca53bcf37af
Offizil <fredoffizil@gmail.com>
2024-01-27 22:32:28 +0100

    w03 programming assignment

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -1,7 +1,7 @@
 /* LESSON 3 - Programming Tasks */
 
 /* FUNCTIONS */
-/* Function Definition - Add Numbers */
+/* Function Definition - Add Numbers *///
 function add(number1, number2) {
     return number1 + number2;
 }
