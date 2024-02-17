1594daa93dfef835152a1ac462ba905779a20045
Offizil <fredoffizil@gmail.com>
2024-02-17 13:48:36 +0100

    update

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,70 +1,87 @@
-let search_button = document.querySelector("#submit_button");
+// let search_button = document.querySelector("#submit_button");
 
-let search_box = document.querySelector("#search_box");
+// let search_box = document.querySelector("#search_box");
 
-const accesstoken = "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";
+// const accesstoken = "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";
 
-let userInput = document.querySelector("#search_box").value;
-
-
+// let userInput = document.querySelector("#search_box").value;
 
 
-// fetch(`https://api.github.com/users/${userInput}`, {
-//   headers: {
-//     Authorization: `Bearer ${accesstoken}`,
-//   },
-// })
-//   .then(response => {
-//     if (!response.ok){
-//       throw new Error('Network respone is not okay');
-//     }
-//    return response.json();
+
+// document.getElementById('search_form').addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   // Check if the pressed key is Enter (key code 13)
+//   if (event.key === 'Enter') {
+//     // Get the input value
+//     userInput = search_box.value;
+
+//     // Make the fetch request
+//     fetch(`https://api.github.com/users/${userInput}`, {
+//       headers: {
+//         Authorization: `Bearer ${accesstoken}`,
+//       },
 //     })
-//   .then(data => console.log(data))
-//   .catch(error => console.error('Error:', error));
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
+//       //   console.log(data);
+//       // })
+//       // .catch(error => {
+//       //   console.error('There was a problem with the fetch operation:', error);
+//       // });
 
 
+//       // Display the user data as JSON in the info_card
+//       document.getElementById('repo').value = data.public_repos;
+//       document.getElementById('followers').value = data.followers;
+//       document.getElementById('following').value = data.following;
+//       })
+//       .catch(error => {
+//         console.error('There was a problem with the fetch operation:', error);
+//       });
 
+    
+//     })
+  
 
-
-
-
-
-//   const userData = async () => {
-//     const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
-//     templeList = await response.json();
-
-//     displayTemples(templeList);
-//     console.log(templeList);
-
-
-// }
+let search_button = document.querySelector("#submit_button");
+let search_box = document.querySelector("#search_box");
+const accessToken = "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";
+let userInput = document.querySelector("#search_box").value;
 
 document.getElementById('search_form').addEventListener('submit', function(event) {
   event.preventDefault();
+  
   // Check if the pressed key is Enter (key code 13)
   if (event.key === 'Enter') {
     // Get the input value
-    // userInput = this.value;
+    userInput = search_box.value;
 
     // Make the fetch request
     fetch(`https://api.github.com/users/${userInput}`, {
       headers: {
-        Authorization: `Bearer ${accesstoken}`,
+        Authorization: `Bearer ${accessToken}`,
       },
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Display the user data as JSON in the info_card
+      document.getElementById('repo').value = data.public_repos;
+      document.getElementById('followers').value = data.followers;
+      document.getElementById('following').value = data.following;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
   }
 });
 
