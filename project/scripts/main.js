c126332b1d61390730a06972f3f5430715587ca5
Offizil <fredoffizil@gmail.com>
2024-02-17 17:48:23 +0100

    work still in progress

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,55 +1,5 @@
-// let search_button = document.querySelector("#submit_button");
-
-// let search_box = document.querySelector("#search_box");
-
-// const accesstoken = "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";
-
-// let userInput = document.querySelector("#search_box").value;
-
-
-
-// document.getElementById('search_form').addEventListener('submit', function(event) {
-//   event.preventDefault();
-//   // Check if the pressed key is Enter (key code 13)
-//   if (event.key === 'Enter') {
-//     // Get the input value
-//     userInput = search_box.value;
-
-//     // Make the fetch request
-//     fetch(`https://api.github.com/users/${userInput}`, {
-//       headers: {
-//         Authorization: `Bearer ${accesstoken}`,
-//       },
-//     })
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//       //   console.log(data);
-//       // })
-//       // .catch(error => {
-//       //   console.error('There was a problem with the fetch operation:', error);
-//       // });
-
-
-//       // Display the user data as JSON in the info_card
-//       document.getElementById('repo').value = data.public_repos;
-//       document.getElementById('followers').value = data.followers;
-//       document.getElementById('following').value = data.following;
-//       })
-//       .catch(error => {
-//         console.error('There was a problem with the fetch operation:', error);
-//       });
-
-    
-//     })
-  
-
-let search_button = document.querySelector("#submit_button");
-let search_box = document.querySelector("#search_box");
+const search_button = document.querySelector("#submit_button");
+const search_box = document.querySelector("#search_box");
 const accessToken = "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";
 let userInput = document.querySelector("#search_box").value;
 
@@ -59,7 +9,7 @@ document.getElementById('search_form').addEventListener('submit', function(event
   // Check if the pressed key is Enter (key code 13)
   if (event.key === 'Enter') {
     // Get the input value
-    userInput = search_box.value;
+    userInput = search_box.value.split(" ").join("");
 
     // Make the fetch request
     fetch(`https://api.github.com/users/${userInput}`, {
@@ -85,3 +35,75 @@ document.getElementById('search_form').addEventListener('submit', function(event
   }
 });
 
+search_button.addEventListener("click", () => {
+  // Get the user name from the search input and remove any spaces
+  userInput = userInput.split(" ").join("");
+
+  
+  // Get the 'user not found' element
+	let userNotFound = document.getElementById("userNotFound");
+
+  // send fetch request to GitHub API, acquire usser info
+  fetch(`https://api.github.com/users/${userInput}`)
+  .then(response => {
+    if (!response.ok) {
+      userNotFound.style.display = "block";
+      throw new Error('Network response was not ok');  //might be rreduntant
+    }
+    else {
+      userNotFound.style.display = "none";
+      return response.json();
+    }
+    
+  })
+  .then(data => {
+
+    let repos = data.public_repos;
+
+
+    let followers = data.followers;
+    document.querySelector("#followers").value = followers;
+
+
+    let following = data.following;
+    document.querySelector("#following").value = followers;
+
+    let datejoined =  data.created_at;
+    document.querySelector("#created_at").value = datejoined;
+
+
+    let updatedAt = data.updated_at;
+    document.querySelector("#updated_at").value = updatedAt;
+
+
+    let organization = data.organization;
+    document.querySelector("#organization").value = organization;
+
+    let profile_pic = data.avatar_url;
+    document.querySelector("profilepic").src = profile_pic;
+
+    
+    let hmtl_profile = data.html_url;
+    document.querySelector("#view_profile").href = hmtl_profile;
+
+    let userid = data.login;
+    document.querySelector("#user_id").value = userid;
+
+
+
+
+   
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
+  
+  
+
+
+
+
+
+
+})
